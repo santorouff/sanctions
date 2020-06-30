ac0d8797acb5224050d88eef3bc0653f9a6f0549
santorouff <santoro_uff@yahoo.com.br>
2020-06-29 21:31:58 -0300

    Update consulta.php

--- consulta.php
+++ consulta.php
@@ -2,37 +2,76 @@
 <html lang="pt-br">
 <head>
 	<meta charset="utf-8"/>
-	<title>Search Result</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">	
+	<title>AmlCheck | Search Result</title>
 	<link rel="stylesheet" type="text/css" href="styles.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="script.js"></script>
 </head>
 <body>
 
 <nav class="topnav">
-  <a class="active" href="home.html">Home</a>
-  <a href="#news">News</a>
-  <a href="#contact">Contact</a>
-  <a href="#about">About</a>
-</nav><br>
+  <a class="active" href="index.html"><i class="fa fa-fw fa-home"></i> Home</a>
+  <a href="#contact"><i class="fa fa-fw fa-phone"></i> Contact</a>
+  <a href="#about"><i class="fa fa-fw fa-user"></i> About</a>
+</nav>
+<br>
 
+<main>
 <fieldset class="campo">
 	<br>
-	<legend><b>Consulte Aqui as Listas de Sanções Internacionais</b></legend>
+	<legend><b>AML Check</b></legend>
 	<form method="get" action="consulta.php">
-		<b>Nome:</b> <input type="text" name="nome" placeholder="Search"/>
-		<input type="submit" value="Procurar" class="botao"/>
+		<b>Entity Name:</b> <input id="name" type="text" name="nome" placeholder="Search"/>
+		<button type="submit" class="botao"><i class="fa fa-fw fa-search"></i></button>
+		<br><br>Similarity: <input type="range" id="similaridade" name="similaridade" min="0" max="100" value="70" class="slider"><span id="similar"></span>%
 	</form>
 </fieldset>
 
+<script>
+		var slider = document.getElementById("similaridade");
+		var output = document.getElementById("similar");
+		output.innerHTML = slider.value;
+		
+		slider.oninput = function() {
+		  output.innerHTML = this.value;
+		}
+</script>
+
+<script>
+function printDiv() { 
+    var divContents = document.getElementById("printable").innerHTML; 
+    var a = window.open('', '', 'height=500, width=500'); 
+    a.document.write('<html>'); 
+    a.document.write('<body >'); 
+    a.document.write(divContents); 
+    a.document.write('</body></html>'); 
+    a.document.close(); 
+    a.print(); 
+}
+</script>
+
 <br/>
 
-<fieldset>
-	<legend><b>Resultado</b></legend>
+<fieldset id="printable">
+	<legend><b>Result</b></legend>
+
+	<b>Today's Date: </b><spam id="time"></spam>
+	
+	<script>
+		document.getElementById("time").innerHTML = new Date();
+	</script>
 
 	<?php
 	$servername = "localhost";
 	$username = "root";
 	$password = "mestrAdo123";
 	$dbname = "sancoes";
+
+//	$servername = "sql202.epizy.com";
+//	$username = "epiz_26089849";
+//	$password = "pJo4o79YZwkDGQg";
+//	$dbname = "epiz_26089849_sancoes";
 
 	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
@@ -42,11 +81,28 @@
 	} 
 
 	$nome = isset($_GET["nome"])?$_GET["nome"]:"";
+	$similaridade = isset($_GET["similaridade"])?$_GET["similaridade"]:"";
 
-	function plevenshtein($str1, $str2) {
-		$lstr1 = strtolower($str1);
-		$lstr2 = strtolower($str2);
-		return number_format((1 - levenshtein($lstr1, $lstr2)/max(strlen($lstr1), strlen($lstr2)))*100,2);
+//	function plevenshtein($str1, $str2) {
+//		$stopwords = ['s.a', 'a.s', 'ltda', 'ltd', 'limited', 'b.v', 'corp.', 'inc.', 's.r.l.', 's.r.o.', 's.a.r.l.', 'de c.v.', 'ooo', 'e.i.r.l.'];
+//		$lstr1 = str_replace($stopwords, "", strtolower($str1));
+//		$lstr2 = str_replace($stopwords, "", strtolower($str2));
+//		return number_format((1 - levenshtein($lstr1, $lstr2)/max(strlen($lstr1), strlen($lstr2)))*100,2);
+//	} 
+
+	function superlevenstein($str1, $str2){
+		$stopwords = ['s.a', 'a.s', 'ltda', 'ltd', 'limited', 'b.v', 'corp.', 'inc.', 's.r.l.', 's.r.o.', 's.a.r.l.', 'de c.v.', 'ooo', 'de c.v.'];
+		$alstr1 = explode(" ", str_replace($stopwords, "", strtolower($str1)));
+		$alstr2 = explode(" ", str_replace($stopwords, "", strtolower($str2)));
+
+		for ($x = 0; sizeof($alstr1); $x++) {
+			$compare = array();
+			for ($y = 0; sizeof($alstr2); $y++){
+				array_push($compare, number_format((1 - levenshtein($alstr1[$x], $alstr2[$x])/max(strlen($alstr1[$x]), strlen($alstr2[$x])))*100,2));
+				$maximo = max($compare);
+				return $maximo;
+			}
+		}
 	}
 	
 	//AS a WHERE levenshtein_ratio(a.SDN_Name, '$nome') > 85 OR a.SDN_Name like '%$nome%'
@@ -58,34 +114,52 @@
 			
 	$result = $conn->query($sql);
 
+	echo "<b> | Term Consulted: </b><spam>'$nome'</spam>";
+	echo "<b> | Similarity: </b><spam>$similaridade%</spam><br><br>";
+
+	echo '<button type="button" onclick="printDiv()"><i class="fa fa-fw fa-download"></i></button><br>';
+	
 	echo "<table>
 		<tr>
 		<th>Entity Name</th>
 		<th>Program</th>
-		<th>Similarity</th>
+		<th>Similarity (%)</th>
 		</tr>";
 		
-	if ($result->num_rows > 0) {
+		$final = array();
 		// output data of each row
 		while($row = $result->fetch_assoc()) {
-			if(plevenshtein($row["SDN_Name"],$nome)>30){
-				echo "<tr><td>".$row["SDN_Name"]."</td>
-				<td>".$row["Program"]."</td>
-				<td>".plevenshtein($row["SDN_Name"],$nome)."%</td></tr>	";
+			if(superlevenstein($row["SDN_Name"],$nome) > $similaridade){
+			//	echo "<tr><td>".$row["SDN_Name"]."</td>
+			//	<td>".$row["Program"]."</td>
+			//	<td>".plevenshtein($row["SDN_Name"],$nome)."%</td></tr>";
+				array_push($final, array("name"=>$row["SDN_Name"], "program"=>$row["Program"], "similarity"=>superlevenstein($row["SDN_Name"],$nome)));
 			}
 		}
-	} else {
-	
-	echo "0 results found";
+
+	//echo "</table>";
+
+	$sim = array_column($final, 'similarity');
+	array_multisort($sim, SORT_DESC, $final);
+
+	foreach($final as $arr){
+	//	echo "<p>".json_encode($arr['name'])."</p>";
+		echo "<tr><td>".str_replace('"', "", json_encode($arr['name']))."</td>";
+		echo "<td>".str_replace('"', "", json_encode($arr['program']))."</td>";
+		echo "<td>".str_replace('"', "", json_encode($arr['similarity']))."</td></tr>";
 	}
 	
 	echo "</table>";
+
+	//if(!$row){echo "0 results found";}
 	
 	$conn->close();
 	?>
 
 </fieldset>
 <br>
-<footer><i><small>Desenvolvido por<br/>Guilherme Santoro<br/>Certified Anti-Money Laudering Specialist&reg</small></i></footer>
+</main>
+
+<footer><i><small>Developed by Guilherme Santoro, Certified Anti-Money Laudering Specialist&reg</small></i></footer>
 </body>
 </html>
